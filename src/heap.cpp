e0c953fc9097668bba36c1009ab9add40c120f9e
reisryan <211036132@aluno.unb.br>
2025-01-28 23:01:37 -0300

    inicio codificação heap

--- src/heap.cpp
+++ src/heap.cpp
@@ -1,1 +1,15 @@
 // Elaboração do Heap da JVM
+#include <string>
+#include <vector>
+#include <unordered_map>
+
+class Object;
+
+class Heap {
+    private:
+        Heap();
+        ~Heap();
+        std::vector<char> _memoryPool;
+        std::unordered_map<size_t, Object*> _objMap;
+        size_t _nextId;
+};
